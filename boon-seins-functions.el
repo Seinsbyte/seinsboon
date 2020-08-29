11e823557136bddb75181c87d867191b864dd8a8
Seinsbyte <ppedalini@gmail.com>
2020-08-29 19:37:12 -0300

    Initial SEINSBOON commit

---
+++ boon-seins-functions.el
@@ -0,0 +1,248 @@
+;; Functions
+
+(defun eval-region-and-deactivate ()
+     (interactive)
+     (when (use-region-p)
+    (eval-region (region-beginning) (region-end)))
+    (boon-deactivate-mark))
+
+    ;; (interactive)
+    ;; (call-interactively (eval-region START END))
+    ;; (boon-deactivate-mark))
+    
+(defun xah-forward-quote-smart ()
+  "Move cursor to the current or next string quote.
+Place cursor at the position after the left quote.
+Repeated call will find the next string.
+URL `http://ergoemacs.org/emacs/emacs_navigating_keys_for_brackets.html'
+Version 2016-11-22"
+  (interactive)
+  (let (($pos (point)))
+    (if (nth 3 (syntax-ppss))
+        (progn
+          (backward-up-list 1 'ESCAPE-STRINGS 'NO-SYNTAX-CROSSING)
+          (forward-sexp)
+          (re-search-forward "\\\"" nil t))
+      (progn (re-search-forward "\\\"" nil t)))
+    (when (<= (point) $pos)
+      (progn (re-search-forward "\\\"" nil t)))))
+
+(defun xah-backward-quote ()
+  "Move cursor to the previous occurrence of \".
+If there are consecutive quotes of the same char, keep moving until none.
+Returns `t' if found, else `nil'.
+URL `http://ergoemacs.org/emacs/emacs_navigating_keys_for_brackets.html'
+Version 2016-07-23"
+  (interactive)
+  (if (re-search-backward "\\\"+" nil t)
+      (when (char-before) ; isn't nil, at beginning of buffer
+        (while (char-equal (char-before) (char-after))
+          (left-char)
+          t))
+    (progn
+      (message "No more quotes before cursor.")
+      nil)))
+
+(defvar xah-brackets nil "string of left/right brackets pairs.")
+(setq xah-brackets "()[]{}<>（）［］｛｝⦅⦆〚〛⦃⦄“”‘’‹›«»「」〈〉《》【】〔〕⦗⦘『』〖〗〘〙｢｣⟦⟧⟨⟩⟪⟫⟮⟯⟬⟭⌈⌉⌊⌋⦇⦈⦉⦊❛❜❝❞❨❩❪❫❴❵❬❭❮❯❰❱❲❳〈〉⦑⦒⧼⧽﹙﹚﹛﹜﹝﹞⁽⁾₍₎⦋⦌⦍⦎⦏⦐⁅⁆⸢⸣⸤⸥⟅⟆⦓⦔⦕⦖⸦⸧⸨⸩｟｠⧘⧙⧚⧛⸜⸝⸌⸍⸂⸃⸄⸅⸉⸊᚛᚜༺༻༼༽⏜⏝⎴⎵⏞⏟⏠⏡﹁﹂﹃﹄︹︺︻︼︗︘︿﹀︽︾﹇﹈︷︸")
+
+(defvar xah-left-brackets '("(" "{" "[" "<" "〔" "【" "〖" "〈" "《" "「" "『" "“" "‘" "‹" "«" )
+  "List of left bracket chars.")
+(progn
+;; make xah-left-brackets based on xah-brackets
+  (setq xah-left-brackets '())
+  (dotimes ($x (- (length xah-brackets) 1))
+    (when (= (% $x 2) 0)
+      (push (char-to-string (elt xah-brackets $x))
+            xah-left-brackets)))
+  (setq xah-left-brackets (reverse xah-left-brackets)))
+
+(defvar xah-right-brackets '(")" "]" "}" ">" "〕" "】" "〗" "〉" "》" "」" "』" "”" "’" "›" "»")
+  "list of right bracket chars.")
+(progn
+  (setq xah-right-brackets '())
+  (dotimes ($x (- (length xah-brackets) 1))
+    (when (= (% $x 2) 1)
+      (push (char-to-string (elt xah-brackets $x))
+            xah-right-brackets)))
+  (setq xah-right-brackets (reverse xah-right-brackets)))
+
+(defun xah-backward-left-bracket ()
+  "Move cursor to the previous occurrence of left bracket.
+The list of brackets to jump to is defined by `xah-left-brackets'.
+URL `http://ergoemacs.org/emacs/emacs_navigating_keys_for_brackets.html'
+Version 2015-10-01"
+  (interactive)
+  (re-search-backward (regexp-opt xah-left-brackets) nil t)
+  (forward-char))
+
+(defun xah-forward-right-bracket ()
+  "Move cursor to the next occurrence of right bracket.
+The list of brackets to jump to is defined by `xah-right-brackets'.
+URL `http://ergoemacs.org/emacs/emacs_navigating_keys_for_brackets.html'
+Version 2015-10-01"
+  (interactive)
+  (re-search-forward (regexp-opt xah-right-brackets) nil t)
+  (backward-char))
+
+(defun xah-delete-blank-lines ()
+  "Delete all newline around cursor.
+URL `http://ergoemacs.org/emacs/emacs_shrink_whitespace.html'
+Version 2018-04-02"
+  (interactive)
+  (let ($p3 $p4)
+          (skip-chars-backward "\n")
+          (setq $p3 (point))
+          (skip-chars-forward "\n")
+          (setq $p4 (point))
+          (delete-region $p3 $p4)))
+
+(defun xah-shrink-whitespaces ()
+  "Remove whitespaces around cursor to just one, or none.
+Shrink any neighboring space tab newline characters to 1 or none.
+If cursor neighbor has space/tab, toggle between 1 or 0 space.
+If cursor neighbor are newline, shrink them to just 1.
+If already has just 1 whitespace, delete it.
+URL `http://ergoemacs.org/emacs/emacs_shrink_whitespace.html'
+Version 2018-04-02T14:38:04-07:00"
+  (interactive)
+  (let* (
+         ($eol-count 0)
+         ($p0 (point))
+         $p1 ; whitespace begin
+         $p2 ; whitespace end
+         ($charBefore (char-before))
+         ($charAfter (char-after ))
+         ($space-neighbor-p (or (eq $charBefore 32) (eq $charBefore 9) (eq $charAfter 32) (eq $charAfter 9)))
+         $just-1-space-p
+         )
+    (skip-chars-backward " \n\t")
+    (setq $p1 (point))
+    (goto-char $p0)
+    (skip-chars-forward " \n\t")
+    (setq $p2 (point))
+    (goto-char $p1)
+    (while (search-forward "\n" $p2 t )
+      (setq $eol-count (1+ $eol-count)))
+    (setq $just-1-space-p (eq (- $p2 $p1) 1))
+    (goto-char $p0)
+    (cond
+     ((eq $eol-count 0)
+      (if $just-1-space-p
+          (delete-horizontal-space)
+        (progn (delete-horizontal-space)
+               (insert " "))))
+     ((eq $eol-count 1)
+      (if $space-neighbor-p
+          (delete-horizontal-space)
+        (progn (xah-delete-blank-lines) (insert " "))))
+     ((eq $eol-count 2)
+      (if $space-neighbor-p
+          (delete-horizontal-space)
+        (progn
+          (xah-delete-blank-lines)
+          (insert "\n"))))
+     ((> $eol-count 2)
+      (if $space-neighbor-p
+          (delete-horizontal-space)
+        (progn
+          (goto-char $p2)
+          (search-backward "\n" )
+          (delete-region $p1 (point))
+          (insert "\n"))))
+     (t (progn
+          (message "nothing done. logic error 40873. shouldn't reach here" ))))))
+
+;; Flyspell previous error
+(defun flyspell-goto-previous-error (arg)
+  "Go to arg previous spelling error."
+  (interactive "p")
+  (while (not (= 0 arg))
+    (let ((pos (point))
+          (min (point-min)))
+      (if (and (eq (current-buffer) flyspell-old-buffer-error)
+               (eq pos flyspell-old-pos-error))
+          (progn
+            (if (= flyspell-old-pos-error min)
+                ;; goto beginning of buffer
+                (progn
+                  (message "Restarting from end of buffer")
+                  (goto-char (point-max)))
+              (backward-word 1))
+            (setq pos (point))))
+      ;; seek the next error
+      (while (and (> pos min)
+                  (let ((ovs (overlays-at pos))
+                        (r '()))
+                    (while (and (not r) (consp ovs))
+                      (if (flyspell-overlay-p (car ovs))
+                          (setq r t)
+                        (setq ovs (cdr ovs))))
+                    (not r)))
+        (backward-word 1)
+        (setq pos (point)))
+      ;; save the current location for next invocation
+      (setq arg (1- arg))
+      (setq flyspell-old-pos-error pos)
+      (setq flyspell-old-buffer-error (current-buffer))
+      (goto-char pos)
+      (if (= pos min)
+          (progn
+            (message "No more miss-spelled word!")
+            (setq arg 0))
+        (forward-word)))))
+
+(defvar xah-recently-closed-buffers nil "alist of recently closed buffers. Each element is (buffer name, file path). The max number to track is controlled by the variable `xah-recently-closed-buffers-max'.")
+
+(defvar xah-recently-closed-buffers-max 40 "The maximum length for `xah-recently-closed-buffers'.")
+
+(defun xah-close-current-buffer ()
+  "Close the current buffer.
+
+Similar to `kill-buffer', with the following addition:
+
+• Prompt user to save if the buffer has been modified even if the buffer is not associated with a file.
+• If the buffer is editing a source file in an org-mode file, prompt the user to save before closing.
+• If the buffer is a file, add the path to the list `xah-recently-closed-buffers'.
+• If it is the minibuffer, exit the minibuffer
+
+URL `http://ergoemacs.org/emacs/elisp_close_buffer_open_last_closed.html'
+Version 2016-06-19"
+  (interactive)
+  (let ($emacs-buff-p
+        ($org-p (string-match "^*Org Src" (buffer-name))))
+
+    (setq $emacs-buff-p (if (string-match "^*" (buffer-name)) t nil))
+
+    (if (string= major-mode "minibuffer-inactive-mode")
+        (minibuffer-keyboard-quit) ; if the buffer is minibuffer
+      (progn
+        ;; offer to save buffers that are non-empty and modified, even for non-file visiting buffer. (because kill-buffer does not offer to save buffers that are not associated with files)
+        (when (and (buffer-modified-p)
+                   (not $emacs-buff-p)
+                   (not (string-equal major-mode "dired-mode"))
+                   (if (equal (buffer-file-name) nil)
+                       (if (string-equal "" (save-restriction (widen) (buffer-string))) nil t)
+                     t))
+          (if (y-or-n-p (format "Buffer %s modified; Do you want to save? " (buffer-name)))
+              (save-buffer)
+            (set-buffer-modified-p nil)))
+        (when (and (buffer-modified-p)
+                   $org-p)
+          (if (y-or-n-p (format "Buffer %s modified; Do you want to save? " (buffer-name)))
+              (org-edit-src-save)
+            (set-buffer-modified-p nil)))
+
+        ;; save to a list of closed buffer
+        (when (buffer-file-name)
+          (setq xah-recently-closed-buffers
+                (cons (cons (buffer-name) (buffer-file-name)) xah-recently-closed-buffers))
+          (when (> (length xah-recently-closed-buffers) xah-recently-closed-buffers-max)
+            (setq xah-recently-closed-buffers (butlast xah-recently-closed-buffers 1))))
+
+        ;; close
+        (kill-buffer (current-buffer))))))
+
+
+
+(provide 'boon-seins-functions)
---
+++ boon-seins-qwerty.el
@@ -0,0 +1,180 @@
+;;; boon-seins-qwerty.el --- An Ergonomic Command Mode  -*- lexical-binding: t -*-
+
+;;; Commentary:
+;;; Code:
+(require 'boon)
+(require 'boon-seins-functions)
+
+;; Select Maps
+
+(define-key boon-select-map "q"  'boon-select-inside-quotes)
+(define-key boon-select-map "Q"  'boon-select-outside-quotes)
+
+(define-key boon-select-map "w"  'boon-select-word)
+
+(define-key boon-select-map "e"  'boon-select-sentence)
+
+(define-key boon-select-map "t"  'boon-select-org-table-cell) ;; Table
+(define-key boon-select-map "T"  'boon-select-org-tree) ;; Tree
+
+(define-key boon-select-map "g"  'boon-select-paragraph)
+
+(define-key boon-select-map "a"  'boon-select-borders) ;; Around
+(define-key boon-select-map "s"  'boon-select-wim) ;; symbol
+(define-key boon-select-map "v"  'boon-select-with-spaces)
+(define-key boon-select-map "d"  'boon-select-document)
+
+(define-key boon-select-map "C"  'boon-select-comment)
+(define-key boon-select-map "x"  'boon-select-outside-pairs) ;; eXpression
+(define-key boon-select-map "c"  'boon-select-inside-pairs) ;; Contents
+
+(define-key boon-select-map "z"  'boon-select-content) ;; inZide
+
+(define-key boon-select-map "b"  'boon-select-blanks) ;; blanKs
+
+;; RIGHT HAND - MOVEMENTS
+
+;; TOP ROW
+
+(define-key boon-moves-map "y"  '("fYnd" . xref-find-definitions))
+(define-key boon-moves-map "Y"  'xref-find-references)
+
+(define-key boon-moves-map "u"  'boon-beginning-of-line)
+
+(define-key boon-moves-map "i"  'previous-line)
+(define-key boon-moves-map "I"  'backward-paragraph)
+
+(define-key boon-moves-map "o"  'next-line)
+(define-key boon-moves-map "O"  'forward-paragraph)
+
+(define-key boon-moves-map "p"  'boon-end-of-line)
+
+;; HOME ROW
+(define-key boon-moves-map "h"  '("hop" . avy-goto-word-1))
+(define-key boon-moves-map "H"  'avy-goto-char)
+
+(define-key boon-moves-map "j" 'backward-word)
+(define-key boon-moves-map "J" 'boon-smarter-backward)
+
+(define-key boon-moves-map "k"  'backward-char)
+(define-key boon-moves-map "K"  'boon-smarter-upward)
+
+(define-key boon-moves-map "l"  'forward-char)
+(define-key boon-moves-map "L"  'boon-smarter-downward)
+
+(define-key boon-moves-map ";" 'forward-word)
+(define-key boon-moves-map ":" 'boon-smarter-forward)
+
+
+;; BOTTOM ROW
+
+(define-key boon-moves-map "n" '("noon walk" . boon-switch-mark))
+(define-key boon-moves-map "N" 'xref-pop-marker-stack)
+
+(define-key boon-moves-map ","  'boon-beginning-of-expression)
+(define-key boon-moves-map "<"  'beginning-of-buffer)
+
+(define-key boon-moves-map "."  'boon-end-of-expression)
+(define-key boon-moves-map ">"  'end-of-buffer)
+
+;; NUMBERS
+(define-key boon-command-map "8"  'xah-backward-quote)
+(define-key boon-command-map "9"  'xah-forward-quote-smart)
+
+(define-key boon-command-map "*"  'xah-backward-left-bracket)
+(define-key boon-command-map "("  'xah-forward-right-bracket)
+
+(define-key boon-command-map "0" 'universal-argument)
+
+
+;; LEFT HAND - COMMANDS
+
+;; TOP ROW
+
+(define-key boon-command-map "q" 'undo)
+(define-key boon-command-map "Q" '("quote" . boon-quote-character))
+
+(define-key boon-moves-map "w" '("where was?" . boon-backward-search-map))
+(define-key boon-moves-map "e" '("elsewhere?" . boon-forward-search-map))
+
+(define-key boon-moves-map "W"  'boon-qsearch-previous)
+(define-key boon-moves-map "E"  'boon-qsearch-next)
+
+(define-key boon-command-map "r" 'swiper-isearch) ; occuR
+(define-key boon-command-map "R" 'kmacro-start-macro) ; Record
+
+
+(define-key boon-command-map "t" '("transform" . boon-replace-by-character))
+
+
+;; HOME ROW
+
+(define-key boon-command-map "a" '("around" . boon-enclose))
+(define-key boon-command-map "A" 'xah-close-current-buffer)
+
+(define-key boon-command-map "s" '("substitute" . boon-substitute-region))
+
+(define-key boon-command-map "d" '("delete" . boon-take-region)) ; "delete"
+(define-key boon-command-map "D" 'boon-treasure-region) ; "duplicate"
+
+(define-key boon-command-map "f" '("fetch" . boon-splice))
+(define-key boon-command-map "F" 'yank-pop)
+
+(define-key boon-command-map "g" '("goto" . boon-goto-map))
+
+;; BOTTOM ROW
+
+(define-key boon-command-map "z" '("repeat" . boon-repeat-command))
+(define-key boon-command-map "Z" 'point-to-register)
+
+(define-key boon-command-map "x" 'boon-x-map)
+(define-key boon-command-map "X" 'jump-to-register)
+
+(define-key boon-command-map "c" 'boon-c-god)
+(define-key boon-command-map "C" 'comment-or-uncomment-region)
+
+(define-key boon-command-map (kbd "C-v") 'boon-open-line-and-insert)
+(define-key boon-command-map "V" 'boon-open-next-line-and-insert)
+(define-key boon-command-map "v" '("v looks like an insert mark" . boon-set-insert-like-state))
+
+(define-key boon-command-map "b" 'insert-register)
+(define-key boon-command-map "B" 'boon-copy-to-register) ; bank
+
+;; NUMBERS
+
+(define-key boon-command-map "1"  'xah-shrink-whitespaces) ; 1 space
+(define-key boon-command-map "2"  'eval-region-and-deactivate)
+(define-key boon-command-map "3"  'split-window-right)
+(define-key boon-command-map "4"  'delete-other-windows)
+
+
+;; Most of these are actually in the boon-moves-map; however some don't quite work there; so they end up here.
+(define-key boon-command-map (kbd "C-k") 'scroll-down-line)
+(define-key boon-command-map (kbd "C-l") 'scroll-up-line)
+
+(define-key indent-rigidly-map "k" 'indent-rigidly-right)
+(define-key indent-rigidly-map "l" 'indent-rigidly-left)
+
+;; Misc crap
+
+(define-key boon-command-map "P" 'kmacro-end-or-call-macro) ; Play
+(define-key boon-command-map "X" 'boon-highlight-regexp)
+(define-key boon-command-map (kbd "<RET>") 'boon-split-line)
+
+;; X-MAPS
+
+(define-key boon-x-map "x" 'counsel-M-x)
+(define-key boon-x-map "C-f" 'counsel-find-file)
+(define-key boon-x-map "f" 'hydra-dired/body)
+(define-key boon-x-map "E" 'eval-region)
+(define-key boon-x-map "c" 'flyspell-buffer)
+(define-key boon-x-map "l" 'my-org-insert-file)
+
+;; SEARCH MAPS
+(define-key boon-forward-search-map "e" 'flyspell-goto-next-error)
+(define-key boon-backward-search-map "e" 'flyspell-goto-previous-error)
+
+
+;;; boon-qwerty.el ends here
+(provide 'boon-seins-qwerty)
+
